95b439a2cba371563c75ef1363de02b16407a739
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-07-20 01:57:06 +0300

    Create HttpStatusChecker.java

---
+++ src/main/java/org/example/HttpStatusChecker.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpStatusChecker {
+    public String getStatusImage(int code) throws IOException {
+        String url = "https://http.cat/" + code + ".jpg";
+        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+        int responseCode = connection.getResponseCode();
+        connection.disconnect();
+        if (responseCode == HttpURLConnection.HTTP_NOT_FOUND) {
+            throw new RuntimeException("don't know this is the code " + code);
+        }
+        return url;
+    }
+}
