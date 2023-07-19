d85269655caab598bdba09521758272b9c407863
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-07-20 01:57:09 +0300

    Create HttpStatusImageDownloader.java

---
+++ src/main/java/org/example/HttpStatusImageDownloader.java
@@ -0,0 +1,41 @@
+package org.example;
+
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+
+public class HttpStatusImageDownloader {
+    public static void main(String[] args) throws IOException {
+        HttpStatusImageDownloader httpStatusImageDownloader = new HttpStatusImageDownloader();
+        httpStatusImageDownloader.downloadStatusImage(404);
+    }
+
+    public void downloadStatusImage(int code) throws IOException {
+        HttpStatusChecker httpStatusChecker = new HttpStatusChecker();
+        String noteror = httpStatusChecker.getStatusImage(code);
+        try {
+
+            URL url = new URL(noteror);
+            URLConnection connection = url.openConnection();
+            InputStream inputStream = connection.getInputStream();
+            FileOutputStream outputStream = new FileOutputStream(code + ".jpg");
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            outputStream.close();
+            inputStream.close();
+        }catch (IOException e){
+            System.out.println("Failed to download image. " + code);
+            e.printStackTrace();
+        }
+    }
+
+}
+
