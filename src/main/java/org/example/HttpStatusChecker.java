c2802f063bcab4db45a326a0e9bb78d94b226731
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-07-21 02:07:51 +0300

    Update HttpStatusChecker.java

--- src/main/java/org/example/HttpStatusChecker.java
+++ src/main/java/org/example/HttpStatusChecker.java
@@ -5,16 +5,21 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class HttpStatusChecker {
-    public String getStatusImage(int code) throws IOException {
+    public String getStatusImage(int code) throws IOException, ImageNotFoundException {
         String url = "https://http.cat/" + code + ".jpg";
         HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
         int responseCode = connection.getResponseCode();
         connection.disconnect();
         if (responseCode == HttpURLConnection.HTTP_NOT_FOUND) {
-            System.out.println("don't know this is the code " + code);
-            url = null;
+            throw new ImageNotFoundException("Image not found for code: " + code);
         }
         return url;
+    }
+
+    public class ImageNotFoundException extends Exception {
+        public ImageNotFoundException(String message) {
+            super(message);
+        }
     }
 
 }
